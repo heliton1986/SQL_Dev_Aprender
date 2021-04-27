9114ea87baff7685a3b824492cf3530b31b14804
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-04-27 14:19:18 -0300

    Add query 008

---
+++ Queries/008.in.sql
@@ -0,0 +1,41 @@
+-- SQL AULA 09 IN
+/*
+ * Nós usamos o operador IN junto com WHERE, para verificar se um valor corresponde
+ * com qualquer valor passado na lista de valores
+ * 
+ * valor IN (valor1, valor2)
+ * valor IN (SELECT valor FROM nomeDaTabela) - Subquery
+ * 
+ * color IN ('blue', 'red')
+ */
+
+SELECT *
+FROM Person.Person;
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID IN (2, 7, 13)
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID NOT IN (2, 7, 13);
+
+-- NÃO USANDO IN
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID = 2 
+OR BusinessEntityID = 7
+OR BusinessEntityID  = 13;
+
+
+
+
+
+
+
+
+
+
+
+
+
